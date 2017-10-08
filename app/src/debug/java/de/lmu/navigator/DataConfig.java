ef5a034e8865c2985e47e4f1b6584eb1ed8450cc
Timo Loewe <timoloewe91@gmail.com>
2017-10-08 14:27:28 +0200

    Update data to v3

--- app/src/debug/java/de/lmu/navigator/DataConfig.java
+++ app/src/debug/java/de/lmu/navigator/DataConfig.java
@@ -4,9 +4,9 @@ public final class DataConfig {
 
     private DataConfig() { }
 
-    public final static int SHIPPED_DATA_VERSION = 2;
+    public final static int SHIPPED_DATA_VERSION = 3;
 
-    public static final String TILES_BASE_PATH = "http://lm00000-rfinder.srv.mwn.de/files/tiles/v2/";
+    public static final String TILES_BASE_PATH = "http://lm00000-rfinder.srv.mwn.de/files/tiles/v3/";
     public static final String PHOTO_BASE_PATH = "http://lm00000-rfinder.srv.mwn.de/files/photos/";
     public static final String THUMBNAIL_BASE_PATH = "http://lm00000-rfinder.srv.mwn.de/files/photos/thumbnails/";
 }
